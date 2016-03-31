c138a23b458aa006053b3bf3b7de1e7efe8e0034
Spencer S <laxscott@gmail.com>
2016-03-31 15:39:21 -0400

    File reading is messed up

--- main.cpp
+++ main.cpp
@@ -1,44 +1,45 @@
 #include<iostream>
 #include<fstream>
-
-#include"bTree.h"
-
+#include<vector>
+#include"Node.cpp"
+#include<string>
 using namespace std;
 
-vector<bTree> readWeights(string fileName);
+void readWeights(string fileName, vector<Node> &weights);
 
 int main(){
 	cout << "Before!" << endl;
 	string input;
 	cout << "Give me a file name for the weights, ya dick: ";
 	cin >> input; 
-	vector<bTree> weights = readWeights(input);
+	vector<Node> weights;
+	readWeights(input,weights);
 
 	for(int i = 0; i < weights.size(); i++){
-		cout << "Item " << i << " is " <<
-			 << weights[i].get_char() <<
-			 << " with a weight of " << 
+		cout << "Item " << i << " is "
+			 << weights[i].get_char()
+			 << " with a weight of "  
 			 << weights[i].get_char() << endl;
 	}
 	cout << "\nAfter!" << endl;	
 	return 0;
 }
 
-vector<bTree> readWeights(string fileName){
+void readWeights(string fileName, vector<Node> &weights){
 	//for line in file
 	ifstream infile;
 	char key;  			//first character is the data for node
 	double weight;		//second string is weight of character 
-	vector<bTree> trees;
 	while(!infile.eof()){
 		string temp;			
 		getline(infile, temp);
+		cout << "This line: " << temp << endl;
 		if(temp != ""){
-			key = temp[:1];
-			weight = stod(temp[2:]);
+			key = temp[0];
+			cout << "Key: " << key << endl;
+			weight = atof(temp.substr(2,temp.size()-1).c_str());
+			Node tempNode = Node(key,weight);
+			weights.push_back(tempNode);
 		}
-		bTree temp = bTree(key,weight);
-		trees.push_back(temp);
 	}
-	return trees;
 }
